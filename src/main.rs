64e8b68b0d937a162e7669d968f0783d1ac3459d
MrWindwaker <lagr45@gmai.com>
2021-06-04 07:27:48 -0500

    Draw Trinagle

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,13 @@ extern crate glium;
 use glium::glutin;
 use glium::Surface;
 
+#[derive(Copy, Clone)]
+struct Vertex{
+    position: [f32; 2]
+}
+
+implement_vertex!(Vertex, position);
+
 fn main() {
     
 
@@ -12,12 +19,45 @@ fn main() {
     let cb = glutin::ContextBuilder::new();
     let display = glium::Display::new(wb, cb, &event_loop).unwrap();
 
+    let vertex1 = Vertex{ position: [-0.5, -0.5] };
+    let vertex2 = Vertex{ position: [0.0, 0.5] };
+    let vertex3 = Vertex{ position: [0.5, -0.25] };
+    let shape = vec![vertex1, vertex2, vertex3];
+
+    let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+
+    let vertex_shader_src = r#"
+        #version 140
+
+        in vec2 position;
+
+        void main(){
+            gl_Position = vec4(position, 0.0, 1.0);
+        }
+    "#;
+
+    let fragment_shader_src = r#"
+        #version 140
+
+        out vec4 color;
+
+        void main(){
+            color = vec4(1.0, 0.0, 0.0, 1.0);
+        }
+    "#;
+
+    let program = glium::Program::from_source(&display, vertex_shader_src, fragment_shader_src, None).unwrap();
+
     event_loop.run(move |ev,_, control_flow|{
         let next_frame_time = std::time::Instant::now() + std::time::Duration::from_nanos(16_666_67);
 
         let mut target = display.draw();
 
         target.clear_color(0.0, 0.0, 1.0, 1.0);
+        // Draw here
+        target.draw(&vertex_buffer, &indices, &program, &glium::uniforms::EmptyUniforms, &Default::default()).unwrap();
+
         target.finish().unwrap();
 
 
